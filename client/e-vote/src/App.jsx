911eb8d18565166b003c175daa8e0798db956217
sudarshan sangle <darshsangle9456@gmail.com>
2025-08-07 14:13:47 +0530

    DONE

---
+++ client/e-vote/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="text-5xl font-bold underline text-blue-600">
+      Hello Tailwind + Vite!
+    </div>
+  );
+}
+
+
+export default App
